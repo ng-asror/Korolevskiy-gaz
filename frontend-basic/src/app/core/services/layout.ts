8c85b637d2056a1b44fbcaa22c4f5b302188c547
Асрор Шодиев <144030637+ng-asror@users.noreply.github.com>
2025-11-29 22:46:36 +0500

    Update layout.ts

--- frontend-basic/src/app/core/services/layout.ts
+++ frontend-basic/src/app/core/services/layout.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, resource } from '@angular/core';
 import { BehaviorSubject, firstValueFrom, Observable, tap } from 'rxjs';
-import { ICallbackRes, IUpdates } from '../interfaces';
+import { ICallbackRes, IRouletteItemsRes, IUpdates } from '../interfaces';
 import { environment } from '../../../environments/environment.development';
 
 @Injectable({
@@ -28,5 +28,18 @@ export class LayoutService {
       `${environment.url}/public/callback-requests`,
       data
     );
+  }
+
+  roulette_items(): Observable<IRouletteItemsRes> {
+    return this.http.get<IRouletteItemsRes>(
+      `${environment.url}/public/roulette/items`
+    );
+  }
+
+  spin(order_id: number, tg_id: string): Observable<any> {
+    return this.http.post<any>(`${environment.url}/public/roulette/items`, {
+      order_id,
+      tg_id,
+    });
   }
 }
